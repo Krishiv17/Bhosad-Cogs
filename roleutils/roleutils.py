0e2ab69d627be307f573b1a83bd6283a4ab5b337
Shukla-G <65753238+Shukla-G@users.noreply.github.com>
2020-10-22 17:41:20 +0530

    aaaaahhh

--- roleutils/roleutils.py
+++ roleutils/roleutils.py
@@ -1,10 +1,10 @@
 from redbot.core import commands
 
-class RoleUnlocking(commands.Cog):
+class RoleUtils(commands.Cog):
     """My custom cog"""
 
     @commands.command()
-    async def unlock1(self, ctx):
+    async def lock1(self, ctx):
         await ctx.channel.set_permissions(ctx.guild.get_role(763959609169674250), send_messages=False)
         """This does stuff!"""
         # Your code will go here
