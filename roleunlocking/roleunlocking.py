1e986a3a5c138737c5e4d8ca1cf3f353ea87e35f
Shukla-G <65753238+Shukla-G@users.noreply.github.com>
2020-10-22 17:32:01 +0530

    pls work

--- roleunlocking/roleunlocking.py
+++ roleunlocking/roleunlocking.py
@@ -5,6 +5,7 @@ class RoleUnlocking(commands.Cog):
 
     @commands.command()
     async def unlock1(self, ctx):
+        await ctx.channel.set_permissions(ctx.guild.get_role(763959609169674250), send_messages=False)
         """This does stuff!"""
         # Your code will go here
-        await ctx.send("I can do stuff!")
+        await ctx.send("Locked channel for <@&763959609169674250>")
