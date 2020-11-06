14c674e147cc0f763ebd98194759baefcbc00c68
Shukla-G <65753238+Shukla-G@users.noreply.github.com>
2020-11-06 17:33:06 +0530

    dsdsdsdsd

---
+++ bhosadutils/__init__.py
@@ -0,0 +1,5 @@
+from .bhosadutils import BhosadUtils
+
+
+def setup(bot):
+    bot.add_cog(BhosadUtils())
--- roleutils/__init__.py
+++
@@ -1,5 +0,0 @@
-from .roleutils import RoleUtils
-
-
-def setup(bot):
-    bot.add_cog(RoleUtils())
