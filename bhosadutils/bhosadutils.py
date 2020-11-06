4122e9cf23df1049f09bdc9a463d2644615b6ca3
Shukla-G <65753238+Shukla-G@users.noreply.github.com>
2020-11-06 17:36:20 +0530

    hmmmmmmm

--- bhosadutils/bhosadutils.py
+++ bhosadutils/bhosadutils.py
@@ -5,8 +5,8 @@ class BhosadUtils(commands.Cog):
     """My custom cog"""
 
     @commands.command()
-    async def lock1(self, ctx):
+    async def lock5(self, ctx):
         await ctx.channel.set_permissions(ctx.guild.get_role(763959609169674250), send_messages=False)
-        """This does stuff!"""
+        """Unlocks Channel for Level 5"""
         # Your code will go here
-        await ctx.send("Locked channel for <@&763959609169674250>")
+        await ctx.send("Locked channel for Level 5 Role")
