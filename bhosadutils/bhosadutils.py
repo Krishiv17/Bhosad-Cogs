0d87c98372e1a6fbfe016bba44f8bee884a44c46
Shukla-G <65753238+Shukla-G@users.noreply.github.com>
2020-11-17 11:50:38 +0530

    didnt work oof

--- bhosadutils/bhosadutils.py
+++ bhosadutils/bhosadutils.py
@@ -12,22 +12,42 @@ class BhosadUtils(commands.Cog):
         await ctx.channel.set_permissions(ctx.guild.get_role(722576062869012521), send_messages=False)
         """Locks Channel for Level 5"""
         await ctx.send("Locked channel for Level 5 Role")
+    @commands.guild_only()
+    @checks.bot_has_permissions(manage_roles=True)
+    @checks.admin_or_permissions(manage_roles=True)
+    @commands.command()
     async def lock10(self, ctx):
         await ctx.channel.set_permissions(ctx.guild.get_role(722576157664608266), send_messages=False)
         """Locks Channel for Level 10"""
         await ctx.send("Locked channel for Level 10 Role")
+    @commands.guild_only()
+    @checks.bot_has_permissions(manage_roles=True)
+    @checks.admin_or_permissions(manage_roles=True)
+    @commands.command()
     async def lock15(self, ctx):
         await ctx.channel.set_permissions(ctx.guild.get_role(722576225020674455), send_messages=False)
         """Locks Channel for Level 15"""
         await ctx.send("Locked channel for Level 15 Role")
+    @commands.guild_only()
+    @checks.bot_has_permissions(manage_roles=True)
+    @checks.admin_or_permissions(manage_roles=True)
+    @commands.command() 
     async def lock20(self, ctx):
         await ctx.channel.set_permissions(ctx.guild.get_role(722576297003450368), send_messages=False)
         """Locks Channel for Level 20"""
         await ctx.send("Locked channel for Level 20 Role")
+    @commands.guild_only()
+    @checks.bot_has_permissions(manage_roles=True)
+    @checks.admin_or_permissions(manage_roles=True)
+    @commands.command()
     async def lock30(self, ctx):
         await ctx.channel.set_permissions(ctx.guild.get_role(730410382585495622), send_messages=False)
         """Locks Channel for Level 30"""
         await ctx.send("Locked channel for Level 30 Role")
+    @commands.guild_only()
+    @checks.bot_has_permissions(manage_roles=True)
+    @checks.admin_or_permissions(manage_roles=True)
+    @commands.command()
     async def lock40(self, ctx):
         await ctx.channel.set_permissions(ctx.guild.get_role(730410698932486195), send_messages=False)
         """Locks Channel for Level 40"""
