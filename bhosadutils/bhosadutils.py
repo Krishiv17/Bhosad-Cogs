bd8486b31e9922411997a2147f5c9ca188ef2050
Shukla-G <65753238+Shukla-G@users.noreply.github.com>
2020-11-17 11:41:26 +0530

    hmm

--- bhosadutils/bhosadutils.py
+++ bhosadutils/bhosadutils.py
@@ -6,16 +6,28 @@ class BhosadUtils(commands.Cog):
 
     @commands.command()
     async def lock5(self, ctx):
-        await ctx.channel.set_permissions(ctx.guild.get_role(763959609169674250), send_messages=False)
+        await ctx.channel.set_permissions(ctx.guild.get_role(722576062869012521), send_messages=False)
         """Locks Channel for Level 5"""
         await ctx.send("Locked channel for Level 5 Role")
     async def lock10(self, ctx):
-        await ctx.channel.set_permissions(ctx.guild.get_role(763959609169674250), send_messages=False)
+        await ctx.channel.set_permissions(ctx.guild.get_role(722576157664608266), send_messages=False)
         """Locks Channel for Level 10"""
         await ctx.send("Locked channel for Level 10 Role")
     async def lock15(self, ctx):
-        await ctx.channel.set_permissions(ctx.guild.get_role(763959609169674250), send_messages=False)
+        await ctx.channel.set_permissions(ctx.guild.get_role(722576225020674455), send_messages=False)
         """Locks Channel for Level 15"""
         await ctx.send("Locked channel for Level 15 Role")
+     async def lock20(self, ctx):
+        await ctx.channel.set_permissions(ctx.guild.get_role(722576297003450368), send_messages=False)
+        """Locks Channel for Level 20"""
+        await ctx.send("Locked channel for Level 20 Role")
+     async def lock30(self, ctx):
+        await ctx.channel.set_permissions(ctx.guild.get_role(730410382585495622), send_messages=False)
+        """Locks Channel for Level 30"""
+        await ctx.send("Locked channel for Level 30 Role")
+      async def lock40(self, ctx):
+        await ctx.channel.set_permissions(ctx.guild.get_role(730410698932486195), send_messages=False)
+        """Locks Channel for Level 40"""
+        await ctx.send("Locked channel for Level 40 Role")
 
 
