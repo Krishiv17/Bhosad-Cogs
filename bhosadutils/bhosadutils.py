85237d7f2678c58e62558768041e207ec89e8582
Shukla-G <65753238+Shukla-G@users.noreply.github.com>
2020-12-07 13:51:36 +0530

    oop

--- bhosadutils/bhosadutils.py
+++ bhosadutils/bhosadutils.py
@@ -10,11 +10,10 @@ class BhosadUtils(commands.Cog):
     @checks.admin_or_permissions(manage_roles=True)
     @commands.command()
     async def lock5(self, ctx):
-        lock_time : 10
         await ctx.channel.set_permissions(ctx.guild.get_role(722576062869012521), send_messages=None)
         """Locks Channel for Level 5"""
         await ctx.send("<a:a_VerifiedBlue:784282245828575282> Locked channel for Level 5 Role")
-        await asyncio.sleep(lock_time)
+        await asyncio.sleep(10)
         await ctx.channel.set_permissions(ctx.guild.get_role(722576062869012521), send_messages=True)
         await ctx.send("<a:a_VerifiedBlue:784282245828575282> Unlocked channel for Level 5 Role")        
         
